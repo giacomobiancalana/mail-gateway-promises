9d380730a632e87fda153d9c2f5180fd75cddc07
giacomo_b <gbiancalana@eagleprojects.it>
2024-04-20 15:20:08 +0000

    funzione da provare nel vero mail gateway: mi manda in already running?

---
+++ comments.ts
@@ -0,0 +1,12 @@
+// FUNZIONE DA PROVARE NEL MAIL GATEWAY VERO: mi manda in already running??
+// Mettila nell'if principale, prima o dopo il job()
+
+// const func = async () => {
+//   await new Promise((res, rej) => {
+//     setTimeout(() => {
+//       console.log("aaa");
+//       res;
+//     }, 3000);
+//   });
+// };
+// func();
