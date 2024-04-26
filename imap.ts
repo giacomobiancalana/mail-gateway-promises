c06dff89f54b92b963cab5d89732a8df0f53359d
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-04-26 14:39:30 +0200

    Refac, esperimenti con uid e altro di FetchQueryObject

--- imap.ts
+++ imap.ts
@@ -1,8 +1,10 @@
-import { ImapFlow, ImapFlowOptions, MailboxLockObject } from 'imapflow';
+import { FetchMessageObject, FetchQueryObject, ImapFlow, ImapFlowOptions, MailboxLockObject } from 'imapflow';
 import path = require('path');
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { getObjWithAccessTokenData } from './tokenGenerator';
+import util from 'util';
+import { ParsedMail, simpleParser } from 'mailparser';  //TODO: lo provi MailParser (al posto di simpleParser)?
 
 const MONITORED_MAIL = process.env.MONITORED_MAIL;
 
@@ -34,48 +36,82 @@ const main = async () => {
   try {
     await client.connect();
   } catch (error) {
-    console.error("Niente connessione, errore:\n", error);
-    return;
+    throw new Error(`Niente connessione, ecco errore:\n${error}`);
   }
-  console.log("CONNESSO!!")
+  console.log("CONNESSO!!");
 
   // Select and lock a mailbox. Throws if mailbox does not exist
   let lock: MailboxLockObject = await client.getMailboxLock('INBOX');
   try {
-    // fetch latest message source
-    // client.mailbox includes information about currently selected mailbox
+    if (client.mailbox === true || client.mailbox === false) {
+      throw new Error("CLIENT.MAILBOX IS A BOOLEAN, I DON'T KNOW WHY");
+    }  // Ora so che client.mailbox è di tipo MailboxObject
+    
     // "exists" value is also the largest sequence number available in the mailbox
-    if (typeof client.mailbox !== 'boolean') {
-      
-      // exists property
-      console.log("client.mailbox.exists:", client.mailbox.exists);
-      
-      // fetchOne method e source
-      let message = await client.fetchOne(`${client.mailbox.exists}`, { source: true });
-      console.log("message.source:", message.source.toString());
+    console.log("Number of messages in this mailbox:", client.mailbox.exists);
+    
+    // fetch latest message source
 
-      // Uid and sequence numbers search
-      const messagesNumbers = await client.search({seen: true, to: 'dev.service+dev@eagleprojects.it'});  // , {uid: true}
-      console.log("messages:\n", messagesNumbers);
-      const messagesNumbersUids = await client.search({seen: true, to: 'dev.service+dev@eagleprojects.it'}, {uid: true});
-      console.log("messages UIDS:\n", messagesNumbersUids);
+    // fetchOne method e source
+    // let message = await client.fetchOne(`${client.mailbox.exists}`, { source: true });
+    // console.log("message.source:", message.source.toString());
 
-      // FetchOne con search soprastante
-      const messageNumb = messagesNumbers[0];
-      console.log("messageNumb:", messageNumb);
-      const mess = await client.fetchOne(`${messageNumb}`, {source: true});
-      //TODO: forse con { uid: true } va a cercare con l'uid invece che con il sequenceNumber (ecco forse perché non funzionava)
-      console.log("mess da fetchare:\n", mess.source.toString());
-      
-      // list subjects for all messages
-      // uid value is always included in FETCH response, envelope strings are in unicode.
-      const yy = client.fetch('1:*', { envelope: true });
-      //TODO: da capire: yy.return; o next() o throw(), e ancora: è un async generator (vedi sotto il "for await")
-      for await (let message of client.fetch('1:*', { envelope: true })) {
-        //TODO: adesso li scandaglia tutti i messaggi, come mai prima non lo faceva??
-        console.log("message uid e altro:\n", `${message.uid}: ${message.envelope.subject}`);
-      }
+
+    // Sequence numbers search
+    const messagesNumbers = await client.search({ seen: true, to: 'dev.service+dev@eagleprojects.it' });
+    console.log("messages seq numbers:\n", messagesNumbers);
+    // Uid search
+    const messagesNumbersUids = await client.search({ seen: true, to: 'dev.service+dev@eagleprojects.it' }, { uid: true });
+    console.log("messages UIDS:\n", messagesNumbersUids);
+    for await (let mess of client.fetch(messagesNumbers, { source: true })) {
+      //TODO: prova anche con opzione uid, passando uid o non facendolo
+      console.log("mess:\n", util.inspect(mess, { showHidden: true, depth: null, colors: true }));
+      console.log("source:", mess.source.toString());
+      console.log("---------------------------------");
+      const parsed = await simpleParser(mess.source);
+      console.log("MAIL PARSATA:\n")
+      console.log(parsed);
+      console.log("#################################");
     }
+
+    // FetchOne con search soprastante
+    const messageNumb = messagesNumbers[0];
+    console.log("messageNumb:", messageNumb);
+    const mess: FetchMessageObject = await client.fetchOne(`${messageNumb}`, { source: true });
+    console.log("mess da fetchare con Seq Number:\n", mess, 'Source:\n', mess.source.toString());
+
+    // Ora fetchOne con uid
+    const messageNumbUid = messagesNumbersUids[0];
+    console.log("messageNumbUid:", messageNumbUid);
+    let fetchQueryObj: FetchQueryObject = {
+      source: true,
+      uid: true,
+      envelope: true,
+      flags: true,
+      bodyStructure: true,
+      internalDate: true,
+      size: true,
+      threadId: true,
+      labels: true,
+      headers: true,
+      // bodyParts: // must be an array of string
+    };
+    const messWithUid: FetchMessageObject = await client.fetchOne(`${messageNumbUid}`, fetchQueryObj, { uid: true });
+    //TODO: forse con { uid: true } va a cercare con l'uid invece che con il sequenceNumber (ecco forse perché non funzionava)
+    console.log("mess da fetchare con UID:\n", messWithUid, '\n', 'SOURCE:\n', messWithUid.source.toString());
+    
+    // list subjects for all messages
+    // uid value is always included in FETCH response, envelope strings are in unicode.
+    // const yy = client.fetch('1:*', { envelope: true });
+    //TODO: da capire: yy.return; o next() o throw(), e ancora: è un async generator (vedi sotto il "for await")
+
+    // for await (let message of client.fetch('1:*', { envelope: true })) {
+    //   //TODO: adesso li scandaglia tutti i messaggi, come mai prima non lo faceva??
+    //   console.log("message uid e altro:\n", `${message.uid}: ${message.envelope.subject}`);
+    // }
+
+  } catch(err) {
+    throw err;
   } finally {
       // Make sure lock is released, otherwise next `getMailboxLock()` never returns
       lock.release();
