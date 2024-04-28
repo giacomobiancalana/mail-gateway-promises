82983772c9d831c87e638cf846f093c588a68da9
Giacomo Biancalana <biancalanagiacomo20@gmail.com>
2024-04-28 11:57:02 +0200

    Per ora, mettoo tutto su app.service, poi vedremo di creare una cartella imap

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { TokenService } from './token/token.service';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnApplicationBootstrap {
+  constructor(public tokenService: TokenService) { }
+
+  response: any;
+  accessToken: string;
+
+  async onApplicationBootstrap() {
+    this.response = await this.main();
+    this.accessToken = this.response["access_token"]
+    console.log(this.accessToken);
   }
+
+  async main() {
+    const res = await this.tokenService.getObjWithAccessTokenData();
+    return res;
+  }
+
 }
